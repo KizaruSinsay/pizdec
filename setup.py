516cab4b816d3bc6c101d29bc4e7df9025dfec57
KizaruSinsay <83453468+KizaruSinsay@users.noreply.github.com>
2023-02-09 13:43:29 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pipisa",
+    version="0.0.1",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gelbpunkt/aiowiki",
+    packages=setuptools.find_packages(),
+)
